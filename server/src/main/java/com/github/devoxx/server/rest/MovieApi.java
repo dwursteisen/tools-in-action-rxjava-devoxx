0e5c51fd9eda877a4bc326f52b8d7c3d580544da
David Wursteisen <david.wursteisen+git@gmail.com>
2015-03-09 17:28:49 +0100

    Ajout d'un service qui crache volontairement

--- server/src/main/java/com/github/devoxx/server/rest/MovieApi.java
+++ server/src/main/java/com/github/devoxx/server/rest/MovieApi.java
@@ -2,17 +2,19 @@ package com.github.devoxx.server.rest;
 
 import java.util.List;
 import java.util.Random;
+
+import com.github.devoxx.server.model.Actor;
+import com.github.devoxx.server.model.Movie;
+import com.github.devoxx.server.model.Synopsis;
+import com.github.devoxx.server.repository.MovieRepository;
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import com.github.devoxx.server.model.Actor;
-import com.github.devoxx.server.model.Movie;
-import com.github.devoxx.server.model.Synopsis;
-import com.github.devoxx.server.repository.MovieRepository;
-import com.wordnik.swagger.annotations.Api;
 
 @Api("Movies")
 @RestController("Movie Controller")
@@ -55,6 +57,15 @@ public class MovieApi {
     @RequestMapping(value = "/movies/translation/{id}/{lang}", method = RequestMethod.GET)
     public Movie translation(@PathVariable("id") String id, @PathVariable("lang") String lang) {
         return repository.translation(id, lang).orElseThrow(() -> new TranslationNotFound(id, lang));
+    }
+
+    @ApiOperation(value = "Ancien service de traduction. Peut planter de temps en temps...")
+    @RequestMapping(value = "/movies/traduction/{id}/{lang}", method = RequestMethod.GET)
+    public Movie traduction(@PathVariable("id") String id, @PathVariable("lang") String lang) {
+        if (id.length() % 2 == 0) {
+            throw new RuntimeException("** BOOM **");
+        }
+        return translation(id, lang);
     }
 
     private void sleep() {
