233f5f7689384d0406b98a8c65b1db82adb05fb6
Brice Dutheil <brice.dutheil@gmail.com>
2015-03-29 23:52:55 +0200

    Adds parallelization slides

---
+++ sandbox/src/main/java/com/github/devoxx/sandbox/panic/J2_Schedulers_Parallelization.java
@@ -0,0 +1,37 @@
+package com.github.devoxx.sandbox.panic;
+
+import java.util.List;
+import com.github.devoxx.sandbox.model.Actor;
+import com.github.devoxx.sandbox.model.Movie;
+import com.github.devoxx.sandbox.model.Page;
+import com.github.devoxx.sandbox.model.Synopsis;
+import com.github.devoxx.sandbox.retrofit.ApiFactory;
+import com.github.devoxx.sandbox.retrofit.ObservableServerApi;
+import com.github.devoxx.sandbox.tooling.Tools;
+import rx.Observable;
+
+public class J2_Schedulers_Parallelization {
+
+    public static void main(String[] args) {
+        ObservableServerApi api = new ApiFactory().reliablePartner().observable();
+
+        api.movies()
+                .flatMap(Observable::from)
+                .flatMap((m) -> api.translation(m.id, "FR"))
+                .flatMap((trad) -> composeMovie(api, trad))
+                .doOnEach(Tools::threadInfo)
+                .flatMap(page ->
+                        Observable.defer(() -> Tools.calculateHeavyStuff(page))
+                                .subscribeOn(Tools.singleThreaded()))
+                .observeOn(Tools.finishScheduler())
+                .forEach(Tools::threadInfo, System.err::println);
+    }
+
+
+    public static Observable<Page> composeMovie(ObservableServerApi api, Movie trad) {
+        Observable<List<Actor>> movieActors = api.actors(trad.id);
+        Observable<Synopsis> movieSynopsis = api.synopsis(trad.id);
+        return Observable.zip(movieActors, movieSynopsis, (actors, synopsis) -> new Page(trad, synopsis, actors));
+    }
+
+}
