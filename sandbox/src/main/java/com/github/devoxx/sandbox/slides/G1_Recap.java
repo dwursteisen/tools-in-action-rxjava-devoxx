4f76caae615ad1a983fd431f64222c2ceb67fb81
David Wursteisen <david.wursteisen+git@gmail.com>
2015-04-03 23:40:23 +0200

    Ajustement

--- sandbox/src/main/java/com/github/devoxx/sandbox/slides/G1_Recap.java
+++ sandbox/src/main/java/com/github/devoxx/sandbox/slides/G1_Recap.java
@@ -7,20 +7,13 @@ import com.github.devoxx.sandbox.retrofit.ObservableServerApi;
 import com.github.devoxx.sandbox.tooling.CSV;
 
 // B, D code si nécessaire
-public class G1_Schedulers {
+public class G1_Recap {
 
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
         ObservableServerApi api = new ApiFactory().reliablePartner().observable();
 
-        // on each print thread info
-        // open CSV -> subscribe and append, print err, close csv
-        CSV csv = new CSV().open();
-
-        // 1) api.movies() -> build page
-        // 2) display thread using :
-        // .doOnEach(Tools::threadInfo)
-        // 3) onCompleted : close and  Tools.printFile(csv.location)
+        // recap
 
         latch.await();
 
