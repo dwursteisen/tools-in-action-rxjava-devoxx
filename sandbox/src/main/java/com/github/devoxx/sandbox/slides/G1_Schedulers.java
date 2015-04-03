15c58a4269c34deadd5c178c57e062dbb2180254
David Wursteisen <david.wursteisen+git@gmail.com>
2015-04-03 17:52:35 +0200

    Add some hints

--- sandbox/src/main/java/com/github/devoxx/sandbox/slides/G1_Schedulers.java
+++ sandbox/src/main/java/com/github/devoxx/sandbox/slides/G1_Schedulers.java
@@ -1,34 +1,29 @@
 package com.github.devoxx.sandbox.slides;
 
-import java.util.List;
-import com.github.devoxx.sandbox.model.Actor;
-import com.github.devoxx.sandbox.model.Movie;
-import com.github.devoxx.sandbox.model.Page;
-import com.github.devoxx.sandbox.model.Synopsis;
+import java.util.concurrent.CountDownLatch;
+
 import com.github.devoxx.sandbox.retrofit.ApiFactory;
 import com.github.devoxx.sandbox.retrofit.ObservableServerApi;
-import rx.Observable;
+import com.github.devoxx.sandbox.tooling.CSV;
 
 // B, D code si nécessaire
 public class G1_Schedulers {
 
     public static void main(String[] args) throws InterruptedException {
+        CountDownLatch latch = new CountDownLatch(1);
         ObservableServerApi api = new ApiFactory().reliablePartner().observable();
 
         // on each print thread info
         // open CSV -> subscribe and append, print err, close csv
+        CSV csv = new CSV().open();
 
-        api.movies()
-                .flatMapIterable(movies -> movies)
-                .flatMap((m) -> api.translation(m.id, "FR"))
-                .flatMap((trad) -> composeMovie(api, trad))
-        ;
+        // 1) api.movies() -> build page
+        // 2) display thread using :
+        // .doOnEach(Tools::threadInfo)
+        // 3) onCompleted : close and  Tools.printFile(csv.location)
+
+        latch.await();
+
     }
 
-
-    public static Observable<Page> composeMovie(ObservableServerApi api, Movie trad) {
-        Observable<List<Actor>> movieActors = api.actors(trad.id);
-        Observable<Synopsis> movieSynopsis = api.synopsis(trad.id);
-        return Observable.zip(movieActors, movieSynopsis, (actors, synopsis) -> new Page(trad, synopsis, actors));
-    }
 }
--- sandbox/src/main/java/com/github/devoxx/sandbox/slides/I_ReactiveStreams.java
+++ sandbox/src/main/java/com/github/devoxx/sandbox/slides/I_ReactiveStreams.java
@@ -2,6 +2,8 @@ package com.github.devoxx.sandbox.slides;
 
 import com.github.devoxx.sandbox.retrofit.ApiFactory;
 import com.github.devoxx.sandbox.retrofit.ObservableServerApi;
+import com.github.devoxx.sandbox.tooling.AwesomeMessageBroker;
+import org.reactivestreams.Publisher;
 
 // conclusion : D
 public class I_ReactiveStreams {
@@ -9,12 +11,10 @@ public class I_ReactiveStreams {
     public static void main(String[] args) throws InterruptedException {
         ObservableServerApi api = new ApiFactory().reliablePartner().observable();
 
+        Publisher<String> publisher = AwesomeMessageBroker.aeron().moviesRefreshCommands();
         // moviesRefresh -> toObservable -> flatMap movies -> flatMap list -> forward
 
-        // AwesomeMessageBroker
+        // Consume in AwesomeMessageBroker
 
-
-
-        // AwesomeMessageBroker
     }
 }
