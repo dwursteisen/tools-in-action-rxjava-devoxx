b1b0be8f5f2d4c65ea4bf7e6df0940907a40950b
David Wursteisen <david.wursteisen+git@gmail.com>
2015-04-08 09:19:54 +0200

    Eneleve le "releable partern"

--- sandbox/src/main/java/com/github/devoxx/sandbox/slides/G1_Recap.java
+++ sandbox/src/main/java/com/github/devoxx/sandbox/slides/G1_Recap.java
@@ -1,17 +1,16 @@
 package com.github.devoxx.sandbox.slides;
 
-import java.util.concurrent.CountDownLatch;
-
 import com.github.devoxx.sandbox.retrofit.ApiFactory;
 import com.github.devoxx.sandbox.retrofit.ObservableServerApi;
-import com.github.devoxx.sandbox.tooling.CSV;
+
+import java.util.concurrent.CountDownLatch;
 
 // B, D code si nécessaire
 public class G1_Recap {
 
     public static void main(String[] args) throws InterruptedException {
         CountDownLatch latch = new CountDownLatch(1);
-        ObservableServerApi api = new ApiFactory().reliablePartner().observable();
+        ObservableServerApi api = new ApiFactory().observable();
 
         // recap
 
